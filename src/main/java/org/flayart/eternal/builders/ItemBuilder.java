c729d18650b887964c80528aaa6f2753575fc1e1
Thundyy <mattiaf764@gmail.com>
2021-07-30 00:00:41 +0200

    Aggiunta possibilità di settare una texture per le teste

--- src/main/java/org/flayart/eternal/builders/ItemBuilder.java
+++ src/main/java/org/flayart/eternal/builders/ItemBuilder.java
@@ -1,43 +1,48 @@
 package org.flayart.eternal.builders;
 
 import com.google.common.collect.Lists;
-import io.netty.util.concurrent.CompleteFuture;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
+import java.lang.reflect.Field;
+import java.util.Base64;
 import java.util.List;
+import java.util.UUID;
 
 public class ItemBuilder {
     
     private final ItemStack itemStack;
     private final ItemMeta itemMeta;
-
+    
     public ItemBuilder(Material material) {
         this.itemStack = new ItemStack(material);
         this.itemMeta = itemStack.getItemMeta();
     }
-
+    
     public ItemBuilder setName(String text) {
         itemMeta.displayName(Component.translatable(text));
         return this;
     }
-
+    
     public ItemBuilder setAmount(int amount) {
         itemStack.setAmount(amount);
         return this;
     }
-
+    
     public ItemBuilder setLore(String... lore) {
         List<Component> componentList = Lists.newArrayList();
         
         for (String s : lore)
             componentList.add(Component.translatable(s));
-    
+        
         itemMeta.lore(componentList);
         return this;
     }
@@ -65,6 +70,36 @@ public class ItemBuilder {
     
     public ItemBuilder addItemFlag(ItemFlag... itemFlags) {
         itemMeta.addItemFlags(itemFlags);
+        return this;
+    }
+    
+    public ItemBuilder setUnbreakable(boolean status) {
+        itemMeta.setUnbreakable(status);
+        return this;
+    }
+    
+    public ItemBuilder setSkull(OfflinePlayer player) {
+        if(itemMeta instanceof SkullMeta)
+            ((SkullMeta) itemMeta).setOwningPlayer(player);
+        return this;
+    }
+    
+    public ItemBuilder setSkull(String url) {
+        if (itemMeta instanceof SkullMeta) {
+            GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+            byte[] encodedData = Base64.getEncoder().encode(String.format("{textures:{SKIN:{url:\"%s\"}}}", url).getBytes());
+            
+            profile.getProperties().put("textures", new Property("textures", new String(encodedData)));
+            Field profileField;
+            try {
+                profileField = ((SkullMeta) itemMeta).getClass().getDeclaredField("profile");
+                profileField.setAccessible(true);
+                profileField.set(itemMeta, profile);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        
         return this;
     }
     
