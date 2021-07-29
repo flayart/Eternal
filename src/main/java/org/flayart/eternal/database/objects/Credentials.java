5fb13dfb9d2b56e64d90c7a96845e36b4baac424
Thundyy <mattiaf764@gmail.com>
2021-07-30 00:46:56 +0200

    Modifiche minori

--- src/main/java/org/flayart/eternal/database/objects/Credentials.java
+++ src/main/java/org/flayart/eternal/database/objects/Credentials.java
@@ -1,6 +1,9 @@
 package org.flayart.eternal.database.objects;
 
 import lombok.Data;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.util.Objects;
 
 @Data
 public class Credentials {
@@ -10,5 +13,22 @@ public class Credentials {
     private final String username;
     private final String password;
     private final boolean useSSL;
-
+    
+    public Credentials(YamlConfiguration file, String configurationSection) {
+        Objects.requireNonNull(file.getConfigurationSection(configurationSection));
+        
+        this.hostname = file.getString(configurationSection + ".hostname");
+        this.database = file.getString(configurationSection + ".database");
+        this.username = file.getString(configurationSection + ".username");
+        this.password = file.getString(configurationSection + ".password");
+        this.useSSL = file.getBoolean(configurationSection + ".useSSL");
+    }
+    
+    public Credentials(YamlConfiguration file, String hostnamePath, String databasePath, String usernamePath, String passwordPath, String useSSLPath) {
+        this.hostname = file.getString(hostnamePath);
+        this.database = file.getString(databasePath);
+        this.username = file.getString(usernamePath);
+        this.password = file.getString(passwordPath);
+        this.useSSL = file.getBoolean(useSSLPath);
+    }
 }
